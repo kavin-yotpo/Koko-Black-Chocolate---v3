ab8bc255767a2f15255c0eb9214bd37030bd9e89
Kavin Abregana <abreganakavin@gmail.com>
2024-11-08 20:27:25 +0800

    init files

--- src/components/ReviewsCarousel.jsx
+++ src/components/ReviewsCarousel.jsx
@@ -46,8 +46,7 @@ function ReviewsCarousel(props) {
             <div
               className="yotpo-widget-instance"
               data-yotpo-instance-id={instanceid}
-              data-yotpo-product-id={productid}
-              mode-preview={productid === "" ? "true" : ""}
+              mode-preview="true"
             ></div>
           </div>
 
